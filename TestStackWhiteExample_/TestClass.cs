b7b287fdc8782050b6c34ac29f206a39284d2fe0
ilaza <ilaza@softserveinc.com>
2020-01-30 16:46:46 +0200

    SetUp created

--- TestStackWhiteExample_/TestClass.cs
+++ TestStackWhiteExample_/TestClass.cs
@@ -1,7 +1,11 @@
 ﻿
 using NUnit.Framework;
+using System.Diagnostics;
 using System.IO;
 using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
 
 namespace TestStackWhiteExample
@@ -9,13 +13,37 @@ namespace TestStackWhiteExample
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void TestMethod()
+        [SetUp]
+        public void SetUp()
         {
+            var ndACButton = Desktop.Instance.Get<Button>(SearchCriteria.ByText("NetDocuments ndOffice"));
+            ndACButton.Click();
 
-            var applicationDirectory = TestContext.CurrentContext.TestDirectory;
-            Application application = Application.Launch("notepad.exe");
-            //Window window = application.GetWindow()
+            var procInfo = new ProcessStartInfo(@"C:\Program Files (x86)\NetDocuments\ndOffice\ndOffice.exe");
+            var application = Application.AttachOrLaunch(procInfo);
+
+            var searchCriteria = SearchCriteria.ByAutomationId("NotificationAreaWindow");
+            var window = application.GetWindow(searchCriteria, InitializeOption.NoCache);
+
+            searchCriteria = SearchCriteria.ByAutomationId("SettingsMenuIcon");
+            var settingMenu = window.Get<Image>(searchCriteria);
+            settingMenu.Click();
+        }
+
+
+        [Test]
+        public void ActivityCenterSettingTest()
+        {
+                    
+
+
+
+
+
+
+
+
+
         }
     }
 }

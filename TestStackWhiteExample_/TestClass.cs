002cba545050b0543307dcae145888faf86c2e95
ilaza <ilaza@softserveinc.com>
2020-01-30 12:28:36 +0200

    Create project

---
+++ TestStackWhiteExample_/TestClass.cs
@@ -0,0 +1,21 @@
+﻿
+using NUnit.Framework;
+using System.IO;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace TestStackWhiteExample
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+
+            var applicationDirectory = TestContext.CurrentContext.TestDirectory;
+            Application application = Application.Launch("notepad.exe");
+            //Window window = application.GetWindow()
+        }
+    }
+}

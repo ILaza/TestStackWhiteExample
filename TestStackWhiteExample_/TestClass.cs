a6fd5537898108fd64a245c1a33494405db4d1c0
ilaza <ilaza@softserveinc.com>
2020-01-31 15:40:51 +0200

    create methods

--- TestStackWhiteExample_/TestClass.cs
+++ TestStackWhiteExample_/TestClass.cs
@@ -1,49 +1,96 @@
-﻿
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System.Diagnostics;
-using System.IO;
+using System.Threading;
+using System.Windows.Automation;
 using TestStack.White;
 using TestStack.White.Factory;
+using TestStack.White.Sessions;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WPFUIItems;
 
 namespace TestStackWhiteExample
 {
     [TestFixture]
-    public class TestClass
+    public class ActivityCenterSettingTestTestClass
     {
-        [SetUp]
-        public void SetUp()
+        private Window ndOficceMainWindow;
+        private Win32Window ndOfficeSettingWindow;
+
+        private const string TAB_OTHER = "Other";
+       // private const int V
+
+        [OneTimeSetUp]
+        public void OpenSettingWindowSetUp()
         {
-            var ndACButton = Desktop.Instance.Get<Button>(SearchCriteria.ByText("NetDocuments ndOffice"));
-            ndACButton.Click();
+            var ndActivityCenterDesctopButton = Desktop.Instance.Get<Button>(SearchCriteria.ByText("NetDocuments ndOffice"));
+            ndActivityCenterDesctopButton.Click();
 
             var procInfo = new ProcessStartInfo(@"C:\Program Files (x86)\NetDocuments\ndOffice\ndOffice.exe");
             var application = Application.AttachOrLaunch(procInfo);
 
             var searchCriteria = SearchCriteria.ByAutomationId("NotificationAreaWindow");
-            var window = application.GetWindow(searchCriteria, InitializeOption.NoCache);
+            var wnd = application.GetWindow(searchCriteria, InitializeOption.NoCache);
+
+            ndOficceMainWindow = new Win32Window(wnd.AutomationElement, WindowFactory.Desktop,
+                                     InitializeOption.NoCache,
+                                     new ApplicationSession().WindowSession(InitializeOption.NoCache));
 
             searchCriteria = SearchCriteria.ByAutomationId("SettingsMenuIcon");
-            var settingMenu = window.Get<Image>(searchCriteria);
-            settingMenu.Click();
-        }
+            var settingImage = ndOficceMainWindow.Get<Image>(searchCriteria);
+            settingImage.Click();
+            Thread.Sleep(1000);
 
+            searchCriteria = SearchCriteria.ByAutomationId("OpenSettingsMenuItem");
+            var settingMenu = ndOficceMainWindow.Get(searchCriteria);
+            settingMenu.Click();
+            GetSettingWindow();
+        }
 
         [Test]
-        public void ActivityCenterSettingTest()
+        public void AOpenTabOtherTest()
         {
-                    
 
+            var tab = ndOfficeSettingWindow.Get(SearchCriteria.ByAutomationId("SettingsTabControl"));
+            var tabOther = tab.Get(SearchCriteria.ByAutomationId("NotificationsTabItem"));
+            tabOther.Click();
+        }
 
-
-
-
-
-
-
+        [Test]
+        public void ChangeSlidersValueTest()
+        {
+            var panelActivityCenter = ndOfficeSettingWindow.Get(SearchCriteria.ByAutomationId("ActivityListGroup"));
+            var slider = panelActivityCenter.Get<Slider>(SearchCriteria.ByAutomationId("NotificationDocumentsNumberSlider"));
+            var thumbSlider = slider.Get<Thumb>(SearchCriteria.ByAutomationId("Thumb"));
+            thumbSlider.SlideHorizontally(-50);
+            Thread.Sleep(1000);
+            thumbSlider.SlideHorizontally(50);
+            Thread.Sleep(1000);
+            slider.Value = 50;
+            Thread.Sleep(1000);
 
         }
+
+        public void GetSettingWindow()
+        {
+            var propCondition = new PropertyCondition(AutomationElement.AutomationIdProperty, "SettingsWindow");
+            var ndOfficeSettingDesctopWindowAutomationElement = ndOficceMainWindow.AutomationElement.FindFirst(TreeScope.Descendants, propCondition);
+            ndOfficeSettingWindow = new Win32Window(ndOfficeSettingDesctopWindowAutomationElement, WindowFactory.Desktop,
+                                                                 InitializeOption.NoCache,
+                                                                 new ApplicationSession().WindowSession(InitializeOption.NoCache));
+            Thread.Sleep(2000);
+        }
+
+        [OneTimeTearDown]
+       public void SettingWindowDeleteTearDown()
+        {
+            if (ndOfficeSettingWindow!= null)
+            {
+                ndOfficeSettingWindow.Close();
+                ndOfficeSettingWindow = null;
+            }
+        }
+            
     }
 }
